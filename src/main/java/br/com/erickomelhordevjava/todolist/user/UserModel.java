f719ad3358502391e922ce1314f3acb494496144
ErickDs-dev <erycdiogenes@gmail.com>
2023-10-10 21:00:40 -0300

    Primeiro POST teste e construção do package users

---
+++ src/main/java/br/com/erickomelhordevjava/todolist/user/UserModel.java
@@ -0,0 +1,31 @@
+package br.com.erickomelhordevjava.todolist.user;
+
+public class UserModel {
+    private String UserName;
+    private String name;
+    private String password;
+
+    public String getUserName() {
+        return UserName;
+    }
+
+    public void setUserName(String userName) {
+        UserName = userName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
