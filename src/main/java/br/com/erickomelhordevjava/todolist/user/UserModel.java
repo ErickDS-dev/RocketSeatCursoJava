83286f83065a1f9bcdc91448ddcb01fd4719dfa3
ErickDs-dev <erycdiogenes@gmail.com>
2023-10-13 23:55:10 -0300

    Criando um banco de dados e aplicando um design melhor na aplicação

--- src/main/java/br/com/erickomelhordevjava/todolist/user/UserModel.java
+++ src/main/java/br/com/erickomelhordevjava/todolist/user/UserModel.java
@@ -1,31 +1,27 @@
 package br.com.erickomelhordevjava.todolist.user;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Entity( name= "tb_users")
 public class UserModel {
-    private String UserName;
+
+@Id
+@GeneratedValue(generator = "UUID")
+    private UUID id;
+
+@Column(unique = true)
+    private String username;
     private String name;
     private String password;
-
-    public String getUserName() {
-        return UserName;
-    }
-
-    public void setUserName(String userName) {
-        UserName = userName;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
+    @CreationTimestamp
+    private LocalDateTime created;
 }
